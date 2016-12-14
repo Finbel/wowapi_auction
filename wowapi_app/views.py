84f205d0e6bc372f4de1c750d989a090a68de1c7
finbel <oregan@kth.se>
2016-12-14 16:59:33 +0100

    switching from python-wowapi

--- wowapi_app/views.py
+++ wowapi_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from wowapi import WowApi
 import os
+import requests
 
 
 
 # Create your views here.
 def front(request):
-	os.environ["WOWAPI_APIKEY"] = "rcbwq4apsrc5qyu28t6ffmzhegpqx2k3"
-	auctions = WowApi.get_auctions('eu', 'Darkmoon%20Faire')
-	string = '{0:30}'.format(str(auctions)[0:30])
+	#os.environ["WOWAPI_APIKEY"] = "rcbwq4apsrc5qyu28t6ffmzhegpqx2k3"
+	#auctions = WowApi.get_auctions('eu', 'Darkmoon%20Faire')
+	url = 'https://eu.api.battle.net/wow/auction/data/darkmoon%20faire?locale=en_GB&apikey=rcbwq4apsrc5qyu28t6ffmzhegpqx2k3'
+	response = requests.get(url)
+	string = '{0:30}'.format(str(response.json())[0:30])
 	return render(request, 'wowapi_app/front.html', {'string' : string})
